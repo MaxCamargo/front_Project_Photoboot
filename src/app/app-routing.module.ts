25ec097b21207c71e3d5d38d7b953a2800fd6b33
Maxiel Camargo <maxielcamargo@gmail.com>
2018-12-16 10:43:00 -0500

    added a new mwnu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', loadChildren: './pages/menu/menu.module#MenuPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'register', loadChildren: './register/register.module#RegisterPageModule' },
 ];
